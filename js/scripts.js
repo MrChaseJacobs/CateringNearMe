3867176253b3b87d609a29e7a3956473ad86caef
Chase Jacobs <chase.jacobs@gmail.com>
2021-04-20 22:50:32 -0500

    nav modals

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,63 @@ $(function () {
     });
 });
 
+$(function () {
+    $("#bookBtnTwo").click(function () {
+        $('#bookModal').modal('toggle');
+    });
+});
+
+$(function () {
+    $("#bookBtnThree").click(function () {
+        $('#eventModal').modal('toggle');
+        $('#bookModal').modal('toggle');
+    });
+});
+$(function () {
+    $("#bookBtnFour").click(function () {
+        $('#menuModal').modal('toggle');
+        $('#bookModal').modal('toggle');
+    });
+});
+$(function () {
+    $("#bookBtnFaq").click(function () {
+        $('#faqModal').modal('toggle');
+        $('#bookModal').modal('toggle');
+    });
+});
+
+$(function () {
+    $("#eventBtn").click(function () {
+        $('#eventModal').modal('toggle');
+    });
+});
+$(function () {
+    $("#faqBtn").click(function () {
+        $('#faqModal').modal('toggle');
+    });
+});
+
+$(function () {
+    $("#menuBtn").click(function () {
+        $('#menuModal').modal('toggle');
+    });
+});
+$(function () {
+    $("#menuBtnTwo").click(function () {
+        $('#menuModal').modal('toggle');
+    });
+});
+$(function () {
+    $("#contactBtn").click(function () {
+        $('#contactModal').modal('toggle');
+    });
+});
+$(function () {
+    $("#contactBtnTwo").click(function () {
+        $('#contactModal').modal('toggle');
+    });
+});
+
 $('.carousel').carousel({
     interval: 3500
   })
